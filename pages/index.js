f986f79528358ef144c84b4b85a2f718f9786a38
santosh-0809 <naiksantosh2448@gmail.com>
2026-08-23 16:40:57 +0530

    Polishing the Index Page implemented successfully

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head';
+import Link from 'next/link';
+
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+
 import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -20,6 +24,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
 
+      {/* Introduction */}
       <section className={utilStyles.headingMd}>
         <p>
           Hi, I'm Santosh Naik. I'm a Computer Engineering student
@@ -31,17 +36,22 @@ export default function Home({ allPostsData }) {
         </p>
       </section>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      {/* Blog Posts */}
+      <section
+        className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+      >
         <h2 className={utilStyles.headingLg}>Blog</h2>
 
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
+
               <br />
-              {id}
-              <br />
-              {date}
+
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
